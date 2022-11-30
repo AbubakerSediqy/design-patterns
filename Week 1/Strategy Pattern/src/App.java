fbce3a98e70e27fa9384c54ffa3eb6eb982b20ef
AbubakerSediqy <45594505+AbubakerSediqy@users.noreply.github.com>
2022-11-30 13:50:16 +0430

    fixed the strategy pattern logic

--- Week 1/Strategy Pattern/src/App.java
+++ Week 1/Strategy Pattern/src/App.java
@@ -3,12 +3,10 @@ import QuackBehavior.Quack;
 
 public class App {
     public static void main(String[] args) throws Exception {
-        Duck md = new MallarDuck();
-        md.display();
-        md.swim();
-        md.setFlyBehavior(new FlyNoWay());
-        md.setQuackBehavior(new Quack());
-        md.performQuack();
-        md.performFly();
+        Duck sd = new SilentDuck(new FlyNoWay(), new Quack());
+        sd.performFly();
+        sd.performQuack();
+        sd.display();
+        sd.swim();
     }
 }
