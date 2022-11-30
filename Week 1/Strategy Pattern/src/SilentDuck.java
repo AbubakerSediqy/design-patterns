4daeb9597d555517ec9feea11a272f985470eb79
AbubakerSediqy <45594505+AbubakerSediqy@users.noreply.github.com>
2022-11-30 13:49:36 +0430

    fixed the strategy pattern logic

--- Week 1/Strategy Pattern/src/SilentDuck.java
+++ Week 1/Strategy Pattern/src/SilentDuck.java
@@ -1,11 +1,27 @@
-public class SilentDuck {
+public class SilentDuck extends Duck {
+
+    Flyable fly;
+    Quackable quack;
+
+    public void SilentDuck(Flyable fly, Quackable quack){\
+        this.fly = fly;
+        this.quack = quack;
+    }
+
+    public voide performFly(){
+        fly.fly();
+    }
+
+    public void performQuack(){
+        quack.quack();
+    }
 
     public void display(){
         System.out.println("I am Silent Duck");
     }
 
     public void swim(){
-        System.out.println("I can not swim");
+        System.out.println("I can swim");
     }
     
 }
